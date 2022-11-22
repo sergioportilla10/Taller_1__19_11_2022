6b098c470c9f739554836b51518090a3a0ac4be2
sergioportilla10 <sergioportilla10@gmail.com>
2022-11-22 02:28:01 -0500

    Solucion 3

--- Forma_Incluir_Javascript_02_19_11_2022/main.js
+++
@@ -1,6 +0,0 @@
-addEventListener(`DOMContentLoaded`, (e)=>{
-
-    console.warn("Conexión con el archivo JavaScript Exitoso")
-    
-    document.querySelector("body").innerHTML = "<h1>Presione F12 para ver informacion en consola</h1> ";
-    })
